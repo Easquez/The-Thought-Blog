0a55a440b59f781673753f6c267da6085b22fde5
Haider Heni <110722921+Easquez@users.noreply.github.com>
2024-02-17 22:37:48 +0100

    Update Loading.jsx

--- frontend/app/blogs/Loading.jsx
+++ frontend/app/blogs/Loading.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 function Loading() {
   return (
     <div className="text-center">
-      <Image src="loading.gif"></Image>
+      <Image src="loading.gif" alt="loading"></Image>
     </div>
   );
 }
